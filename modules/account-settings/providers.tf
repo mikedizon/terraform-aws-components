38dcdc794035b478cce91c9554e3e7acc08ddeed
Michael Dizon <mdizon@gmail.com>
2022-04-16 12:45:10 -0400

    update(account-settings): add arn_format variable

--- modules/account-settings/providers.tf
+++ modules/account-settings/providers.tf
@@ -13,10 +13,17 @@ module "iam_roles" {
   source     = "../account-map/modules/iam-roles"
   privileged = true
   context    = module.this.context
+  arn_format = var.arn_format
 }
 
 variable "import_role_arn" {
   type        = string
   default     = null
   description = "IAM Role ARN to use when importing a resource"
+}
+
+variable "arn_format" {
+  type        = string
+  default     = "arn:aws"
+  description = "ARN format to be used. May be changed to support deployment in GovCloud/China regions"
 }
